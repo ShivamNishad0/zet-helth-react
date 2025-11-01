b48fea58b54681514de2d5843a320e1c4ea5f868
shivamnishad0 <2k23.mca2313672@gmail.com>
2025-11-01 12:40:15 +0530

    fix

--- zethealth-main-main/android/app/src/main/kotlin/com/healthexpress/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.healthexpress
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- zethealth-main-main/lib/Screens/BookingScreen/MapScreen/Stages.dart
+++
@@ -1,7 +0,0 @@
-class Stages {
-  final String status;
-  final DateTime time;
-  final bool isCompleted;
-
-  Stages(this.status, this.time, this.isCompleted);
-}
